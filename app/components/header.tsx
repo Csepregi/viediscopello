da48a64639a4f19bd39e1a6f82c28032f64af225
csepregigabor <gabor.csepregi@accenture.com>
2024-05-06 00:32:36 +0200

    tastavino correct link at mobile view

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -36,7 +36,7 @@ export default function Header () {
         {isMenuOpen && (
        <div className="lg:hidden">
        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-         <a href="/wineTasting" className="block px-3 py-2 rounded-md text-base font-medium text-blue hover:text-blue-800 hover:bg-gray-50">Tastavino</a>
+         <a href="/tastavino" className="block px-3 py-2 rounded-md text-base font-medium text-blue hover:text-blue-800 hover:bg-gray-50">Tastavino</a>
        </div>
      </div>
         )}

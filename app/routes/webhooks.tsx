16b65d879c9331ef72d5f812f4efc901d6fdd627
csepregigabor <gabor.csepregi@accenture.com>
2024-06-14 10:21:45 +0200

    delete secret

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -9,23 +9,4 @@ export const action = async ({request}) => {
   } catch (error) {
     return error
   }
-  // const secret = 'whsec_c81d7aa08c421c066c1cafeb9c5542f6535b90afd1a6f1f5c9b12e4edb49868e'
-  // const sig = request.headers.get('stripe-signature')
-  // let event;
-  // const payload = await request.text()
-
-  // try {
-  //   event = stripe.webhooks.constructEvent(payload, sig, secret)
-  // } catch(err) {
-  //   return new Response(err.message, {
-  //     status: 400,
-  //   })
-  // }
-
-  // if(event.type == 'payment_intent.succeeded') {
-  //   const paymentIntent = event.data.object;
-  //   console.log("💰 payment success!")
-  // }
-
-  // return {};
 }

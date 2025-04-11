2519d8f9ae922d060bd28cbf777b206d5c378522
csepregigabor <gabor.csepregi@accenture.com>
2025-04-11 11:03:54 +0200

    fly.toml edit

--- start.sh
+++ start.sh
@@ -1,18 +1,15 @@
-#!/bin/sh -ex
+#!/bin/sh
 
-# Create data directory if it doesn't exist
-mkdir -p /data
+# Attempt to create swap (but don't fail if it doesn't work)
+fallocate -l 512M /swapfile || true
+chmod 0600 /swapfile || true
+mkswap /swapfile || true
+echo 10 > /proc/sys/vm/swappiness || true
+swapon /swapfile || true
+echo 1 > /proc/sys/vm/overcommit_memory || true
 
-# allocate swap space
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
-
-# Ensure database migrations are run
+# Run prisma migrations
 npx prisma migrate deploy
 
-# Start the application
-npm run start
+# Start the application with explicit host and port
+HOST=0.0.0.0 PORT=3000 exec node ./build/server/index.js
